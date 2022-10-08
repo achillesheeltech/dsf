bd0e44c0626eb318c153e7c8ff0146df759de762
Brian Brown <brianbrown@Brians-MacBook-Pro-6.local>
2022-10-08 00:17:07 -0600

    added new tests for dsf_test.py

--- dsf_test.py
+++ dsf_test.py
@@ -4,7 +4,7 @@ import dsf
 
 
 class MyTestCase(unittest.TestCase):
-    def test_low_ordered_ret_PQ(self):
+    def test_one_ordered_ret_PQ(self):
 
         # Low ordered system (works)
         A = np.array([[0.5]])
@@ -14,28 +14,81 @@ class MyTestCase(unittest.TestCase):
 
         index = [0]
 
-        # print(index)
-        print("A.shape", A.shape)
-        print("B.shape", B.shape)
-        print("C.shape", C.shape)
-        print("D.shape", D.shape)
-
         p, q = dsf.ret_PQ(A, B, C, D, index)
-
-        # print("P", p)
-        # print("Q", q)
 
         self.assertIsNotNone(p)
         self.assertIsNotNone(q)
         # self.assertEqual(True, False)  # add assertion here
 
-    # def test_two_ordered_ret_PQ(self):
-    # A higher order system (not yet working)
-    # A = np.array([[0.5, 0, 0, 0], [0, 0.5, 0, 0], [0, 0, 0.5, 0], [0, 0, 0, 0.5]])
-    # B = np.array([[1], [1], [1], [1]])
-    # C = np.array([[1, 1, 1, 1]])
-    # D = np.array([1])
-    # index = [0]
+    def test_two_ordered_ret_PQ(self):
+
+        # Low ordered system (works maybe)
+        A = np.array([[0.5, 0], [0, 0.5]])
+        B = np.array([[1, 1],[1,1]])
+        C = np.array([[1], [1]])
+        D = np.array([[1, 1], [1, 1]])
+
+        index = [0]
+
+        p, q = dsf.ret_PQ(A, B, C, D, index)
+
+        self.assertIsNotNone(p)
+        self.assertIsNotNone(q)
+
+        # self.assertEqual(True, False)  # add assertion here
+
+    def test_three_ordered_ret_PQ(self):
+
+        # Low ordered system (works maybe)
+        A = np.array([[0.5, 0, 0], [0, 0.5, 0], [0, 0, 0.5]])
+        B = np.array([[1, 1,1],[1,1,1], [1,1,1]])
+        C = np.array([[1], [1], [1]])
+        D = np.array([[1, 1,1], [1, 1,1], [1,1,1]])
+
+        index = [0]
+
+        p, q = dsf.ret_PQ(A, B, C, D, index)
+
+        self.assertIsNotNone(p)
+        self.assertIsNotNone(q)
+
+        # self.assertEqual(True, False)  # add assertion here
+
+    def test_four_ordered_ret_PQ(self):
+
+        # Low ordered system (works maybe)
+        A = np.array([[0.5, 0, 0, 0], [0, 0.5, 0, 0], [0, 0, 0.5, 0], [0, 0, 0, 0.5]])
+        B = np.array([[1, 1,1,1],[1,1,1,1], [1,1,1,1], [1,1,1,1]])
+        C = np.array([[1], [1], [1], [1]])
+        D = np.array([[1, 1,1,1], [1, 1,1,1], [1,1,1,1], [1,1,1,1]])
+
+        index = [0]
+
+        p, q = dsf.ret_PQ(A, B, C, D, index)
+
+        self.assertIsNotNone(p)
+        self.assertIsNotNone(q)
+
+        # self.assertEqual(True, False)  # add assertion here
+
+
+    def test_five_ordered_ret_PQ(self):
+
+        # Low ordered system (works maybe)
+        A = np.array([[0.5, 0, 0, 0,0], [0, 0.5, 0, 0,0], [0, 0, 0.5, 0,0], [0, 0, 0, 0.5,0], [0, 0, 0, 0,0.5]])
+        B = np.array([[1,1,1,1,1],[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1]])
+        C = np.array([[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1]])
+        D = np.array([[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1]])
+
+        index = [1,2,3]
+
+        p, q = dsf.ret_PQ(A, B, C, D, index)
+
+
+        self.assertIsNotNone(p)
+        self.assertIsNotNone(q)
+		
+        # self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == "__main__":
